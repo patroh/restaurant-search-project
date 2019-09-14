e29bfa8bf44ac2121bf982bd4c13ebdabc6b6795
Rohan Patel <rohanpatel7777777@gmail.com>
2019-09-14 12:49:43 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express=require("express");
+var app=express();
+var indexRoutes=require("./routes/indexRoutes");
+var restaurentRoutes=require("./routes/restaurentRoutes");
+
+app.set("view engine","ejs");
+app.use(express.static(__dirname));
+app.use(indexRoutes);
+app.use(restaurentRoutes);
+
+app.listen(3000,function () {
+   console.log("SERVER STARTED PORT 3000");
+});
---
+++ routes/indexRoutes.js
@@ -0,0 +1,32 @@
+var express=require("express");
+var router=express.Router();
+var request=require("request");
+const API_KEY="fPqlQMDMDqFDnUHhNr1vq__LEXSHMUxCc3nYLULklv33jFfC6-1scdyMPlFwwNPLTCJIbV-RRzlqXwcV_-3AQ8l1e5RgVAEQnEJjS8oZzD0FzQIEC9AqXDrFb6R3XXYx";
+
+router.get("/",function (req,res) {
+    res.render("home");
+});
+router.get("/search",function (req,res) {
+    const SEARCH_URL="https://api.yelp.com/v3/businesses/search?";
+    var term=req.query.query;
+    var lat=req.query.lat;
+    var lon=req.query.lon;
+    var option={
+      url:  SEARCH_URL+"term="+term+"&latitude="+lat+"&longitude="+lon+"&limit=50",
+        headers:{
+          "authorization":"bearer "+API_KEY
+        }
+    };
+    request(option,function (err,response,body) {
+       if(err){
+           console.log(err);
+       }
+       else {
+           //res.send(JSON.parse(body));
+           res.render("restaurents",{restaurents:JSON.parse(body).businesses});
+       }
+    });
+});
+
+
+module.exports=router;
